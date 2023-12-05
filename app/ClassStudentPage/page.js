f27b808b7198a49028a68ce3444a21aac5bc2b7b
Jin <jin.her@edu.sait.ca>
2023-12-04 18:48:06 -0700

    Simple Navigation Added

---
+++ app/ClassStudentPage/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../navbar"
+
+export default function Page() {
+    return (
+        <div>
+            <Navbar />
+
+            <main>
+                <h1>ClassStudentPage</h1>
+            </main>
+        </div>
+    )
+}
---
+++ app/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
+import './Styles/style.css';
+
+export default function Navbar() {
+  const webPages = ["Login", "ClassStudentPage"];
+
+  return (
+    <nav className="navbar">
+      <ul className="flex items-center justify-between space-x-4">
+        {webPages.map((page) => (
+          <li key={page} className="hover:text-green-400 hover:underline">
+            <Link href={`/${page}`}>{page}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
