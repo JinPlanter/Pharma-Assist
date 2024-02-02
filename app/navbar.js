08c038a4cdf1a285ce3fb69c6f5be669aa2d21c1
Sergio Martinez <maikiby123@gmail.com>
2024-02-02 06:40:14 -0700

    Add logo to navbar

--- app/navbar.js
+++ app/navbar.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import Image from 'next/image';
 // import './Styles/style.css';
 
 export default function Navbar() {
@@ -9,14 +10,18 @@ export default function Navbar() {
     const pathname = usePathname();
 
     return (
-        <nav className="navbar">
-            <ul className="flex items-center justify-between space-x-4">
-                {webPages.map((page) => (
-                    <li key={page} className={`block py-2.5 px-4 rounded transition duration-200 link ${pathname === `/Tabs/${page}` ? 'text-green-400 bg-slate-200' : 'hover:text-green-400 hover:underline'}`}>
-                        <Link href={`/Tabs/${page}`}>{page}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <div className='flex'>
+            <Image src="/logo.png" alt="logo" width={72} height={72} href="/" className='flex-none'/>
+            <nav className="navbar flex-grow">
+                <ul className="flex items-center justify-between space-x-4">
+                    {webPages.map((page) => (
+                        <li key={page} className={`flex-grow block py-2.5 px-4 rounded transition duration-200 link ${pathname === `/Tabs/${page}` ? 'text-green-400 bg-slate-200' : 'hover:text-green-400 hover:underline'}`}>
+                            <Link href={`/Tabs/${page}`}>{page}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+
     )
 }
