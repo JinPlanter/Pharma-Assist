32c11498392d5aeb62b6eb853b5bdaff78c14777
Sergio Martinez <maikiby123@gmail.com>
2024-02-06 23:38:20 -0700

    Changed navbar orientation

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,80 +1,82 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Navigation() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
-    const toggleDrawer = () => {
-        setIsDrawerOpen(!isDrawerOpen);
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
+  {
+    /* Close Drawer when user clicks outside of it */
+  }
+  useEffect(() => {
+    if (isDrawerOpen) {
+      document.addEventListener("click", closeDrawer);
+    }
+    return () => {
+      document.removeEventListener("click", closeDrawer);
     };
-
-    const closeDrawer = () => {
-        setIsDrawerOpen(false);
-    };
-
-    {/* Close Drawer when user clicks outside of it */ }
-    useEffect(() => {
-        if (isDrawerOpen) {
-            document.addEventListener('click', closeDrawer);
-        }
-        return () => {
-            document.removeEventListener('click', closeDrawer);
-        };
-    }, [isDrawerOpen]);
-
-
-    return (
-        <nav className="navigation">
-            <div className="navigationContainer">
-                {/* Hamburger Button for Small Screens */}
-                <button
-                    className="lg:hidden"
-                    onClick={toggleDrawer}
-                >
-                    <span className="text-2xl">&#9776;</span>
-                </button>
-
-                {/* Logo */}
-                <div className="logo">
-                    Pharma Assist
-                </div>
-
-                {/* Drawer for Small Screens */}
-                {isDrawerOpen && (
-                    <div className="lg:hidden navigationDrawer">
-                        <ul className="navigationDrawerUl">
-                            <li className='navigationDrawerLi'>
-                                <Link className='navigationDrawerLink' href="/" onClick={toggleDrawer}>Home</Link>
-                            </li>
-                            <li className='navigationDrawerLi'>
-                                <Link className='navigationDrawerLink' href="/Login" onClick={toggleDrawer}>Login</Link>
-                            </li>
-                            <li className='navigationDrawerLi'>
-                                <Link className='navigationDrawerLink' href="/ImportFile" onClick={toggleDrawer}>Import File</Link>
-                            </li>
-                            <li className='navigationDrawerLi'>
-                                <Link className='navigationDrawerLink' href="/Students" onClick={toggleDrawer}>Students</Link>
-                            </li>
-                            <li className='navigationDrawerLi'>
-                                <Link className='navigationDrawerLink' href="/Grading" onClick={toggleDrawer}>Grading</Link>
-                            </li>
-                        </ul>
-                    </div>
-                )}
-
-                {/* Horizontal Navigation for Large Screens */}
-                <div className="hidden lg:flex space-x-4">
-                    <Link href="/">Home</Link>
-                    <Link href="/Login">Login</Link>
-                    <Link href="/ImportFile">Import File</Link>
-                    <Link href="/Students">Students</Link>
-                    <Link href="/Grading">Grading</Link>
-                </div>
-
-            </div>
-        </nav>
-    )
+  }, [isDrawerOpen]);
+  {/* TODO: Make the navbar collapsable on mobile devices */}
+  return (
+    <nav className="navigation">
+      <div className="navigationDrawer">
+        <ul className="navigationDrawerUl">
+          <li className="navigationDrawerLi">
+            <Link
+              className="navigationDrawerLink"
+              href="/"
+              onClick={toggleDrawer}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="navigationDrawerLi">
+            <Link
+              className="navigationDrawerLink"
+              href="/Login"
+              onClick={toggleDrawer}
+            >
+              Login
+            </Link>
+          </li>
+          <li className="navigationDrawerLi">
+            <Link
+              className="navigationDrawerLink"
+              href="/ImportFile"
+              onClick={toggleDrawer}
+            >
+              Import File
+            </Link>
+          </li>
+          <li className="navigationDrawerLi">
+            <Link
+              className="navigationDrawerLink"
+              href="/Students"
+              onClick={toggleDrawer}
+            >
+              Students
+            </Link>
+          </li>
+          <li className="navigationDrawerLi">
+            <Link
+              className="navigationDrawerLink"
+              href="/Grading"
+              onClick={toggleDrawer}
+            >
+              Grading
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,24 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 //import '../Styles/style.css';
-import Navigation from './components/navigation'
-const inter = Inter({ subsets: ['latin'] })
+import Navigation from "./components/navigation";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Pharmacy Grading Tool',
-  description: 'Pharmacy Grading Tool for the Pharmacy Assistant Program at SAIT.',
-}
+  title: "Pharmacy Grading Tool",
+  description:
+    "Pharmacy Grading Tool for the Pharmacy Assistant Program at SAIT.",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className='m-5 {inter.className}'>
-        <Navigation/>
-        <div>{children}</div>
+    <html lang="en" className="h-screen">
+      <body className="{inter.className}">
+        <div className="flex h-screen">
+          <Navigation />
+          <div>{children}</div>
+        </div>
       </body>
     </html>
-  )
+  );
 }
