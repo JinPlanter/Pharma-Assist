d54c4a6c1fdac259a51fb07e34418229e3833907
izaizaiza <117947026+izaizaiza@users.noreply.github.com>
2024-02-08 14:56:07 -0700

    added change of color to classlist item to show student is selected

--- app/components/classList.js
+++ app/components/classList.js
@@ -1,8 +1,19 @@
 // Import the React module
-import React from "react";
+import React, { useState } from "react";
 
 // ClassList component that takes in classlist and onStudentClick as props
 const ClassList = ({ classlist, onStudentClick }) => {
+  // make li highlighted once clicked
+  const [selectedStudent, setSelectedStudent] = useState(null);
+
+  // Function to handle the click event on a student
+  const handleClick = (student) => {
+    // Call the onStudentClick function passed in as a prop
+    onStudentClick(student);
+    // Set the selected student to the student that was clicked
+    setSelectedStudent(student);
+  };
+
   // JSX rendering for the ClassList component
   return (
     <ul className="classList">
@@ -12,8 +23,8 @@ const ClassList = ({ classlist, onStudentClick }) => {
       {classlist.map((student) => (
         <li
           key={student.id}
-          className="classListItem"
-          onClick={() => onStudentClick(student)}
+          className={`classListItem ${selectedStudent && selectedStudent.id === student.id ? "selected" : ""}`}
+          onClick={() => handleClick(student)}
         >
           {/* Display the name of the student */}
           <span className="classListStudent">
