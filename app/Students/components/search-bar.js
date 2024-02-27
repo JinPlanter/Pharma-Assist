8f00e521952927a570fa9d132254f98a3ca0bc7b
izalu99 <>
2024-02-27 14:34:36 -0700

    adjusted scores by most unique identifier; clean up code

--- app/Students/components/search-bar.js
+++ app/Students/components/search-bar.js
@@ -1,22 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const SearchBar = ({ data }) => {
     const [searchInput, setSearchInput] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     
-    
-    const handleSearchInput = (event) => {
-        event.preventDefault();
-        setSearchInput(event.target.value);
+    useEffect(() => {
+        handleSearchInput();
+    }, [searchInput]);
 
+
+    // score search results by category, top score is displayed on top of list
+    // most unique identifier: studentid > class > name
+    const calculateScore = (val) => {
+        const nameScore = val.name.toLowerCase().includes(searchInput.toLowerCase()) ? 1 : 0;
+        const classScore = val.class.toLowerCase().includes(searchInput.toLowerCase()) ? 2 : 0;
+        const studentIdScore = val.studentid.includes(searchInput) ? 3 : 0; // most unique identifier thus highest score
+
+        return nameScore + classScore + studentIdScore;
+    }
+    
+
+    const handleSearchInput = () => {
         if (searchInput.length > 0) {
-            const results = data.filter((val) => {
-                return(
-                val.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-                val.class.toLowerCase().includes(searchInput.toLowerCase()) ||
-                val.studentid.includes(searchInput)
-                );
-            });
+            const results = data
+            .map((val) => ({...val, score: calculateScore(val)}))
+            .filter((val) => val.score > 0)
+            .sort((a, b) => b.score - a.score);
             setSearchResults(results);
             console.log("search results: ",searchResults);
         }
@@ -27,10 +36,16 @@ const SearchBar = ({ data }) => {
     };
 
 
-    
+
     return (
         <div>
-            <input className = "text-black" type="text" placeholder="Search..." value={searchInput} onChange={handleSearchInput} />
+            <input 
+            className = "text-black" 
+            type="text" 
+            placeholder="Search..." 
+            value={searchInput} 
+            onChange={(event)=> setSearchInput(event.target.value)} 
+            />
             {searchResults.length > 0 && (
             <ul>
                 {searchResults.map((result) => (
