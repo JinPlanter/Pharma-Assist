fc60a8bd15eb14539a08b14d1f373e06945d1c8f
Sergio Martinez <maikiby123@gmail.com>
2024-03-12 14:56:34 -0600

    fix navbar not rendering full height

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
       <body className={`{inter.className} h-full`}>
-        <div>{children}</div>
+        <div className="h-full">{children}</div>
       </body>
     </html>
   );
