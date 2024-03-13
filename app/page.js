ac157aceeb9fe604b0465a5eecf9a43092c1d467
Sergio Martinez <maikiby123@gmail.com>
2024-03-13 10:55:09 -0600

    made basic homepage to iterate on colors

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
-"use client";
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <>
-      <Link className="btn btn-primary" href="/dashboard">
-        Dashboard
-      </Link>
-    </>
+    <div className="flex flex-col h-screen">
+      <header className="p-4 bg-primary text-white">Header</header>
+      <div className="flex flex-grow">
+        <main className="flex-grow p-4 bg-base-100 text-black">
+          <button className="btn btn-primary">Primary</button>
+          <button className="btn btn-secondary">Secondary</button>
+          <button className="btn btn-accent">Accent</button>
+          <button className="btn btn-neutral">Neutral</button>
+          <button className="btn btn-info">Info</button>
+          <button className="btn btn-success">Success</button>
+          <button className="btn btn-warning">Warning</button>
+          <button className="btn btn-error">Error</button>
+          <button className="btn btn-ghost">Ghost</button>
+        </main>
+      </div>
+      <footer className="p-4 bg-primary text-white">Footer</footer>
+    </div>
   );
 }
