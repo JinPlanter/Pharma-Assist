cdb6f402bf263bd1acb1c5ed8818558b0aebd487
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2024-03-16 17:34:05 -0600

    main page
    Main page rendering the login page

--- app/page.js
+++ app/page.js
@@ -1,21 +1,39 @@
+"use client";
+
+import React, { useState } from "react";
+import LoginPage from "./Login/page.js";
+
 export default function Home() {
+  const [showLoginPage, setShowLoginPage] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowLoginPage(true);
+  };
+
   return (
-    <div className="flex flex-col h-screen">
-      <header className="p-4 bg-primary text-white">Header</header>
-      <div className="flex flex-grow">
-        <main className="flex-grow p-4 bg-base-100 text-black">
-          <button className="btn btn-primary">Primary</button>
-          <button className="btn btn-secondary">Secondary</button>
-          <button className="btn btn-accent">Accent</button>
-          <button className="btn btn-neutral">Neutral</button>
-          <button className="btn btn-info">Info</button>
-          <button className="btn btn-success">Success</button>
-          <button className="btn btn-warning">Warning</button>
-          <button className="btn btn-error">Error</button>
-          <button className="btn btn-ghost">Ghost</button>
-        </main>
+    <section class="bg-center bg-no-repeat bg-[url('https://sundancecollege.com/uploads/Blog/community-pharmacy-in-Calgary.jpg')] bg-gray-700 bg-blend-multiply">
+      <div class="px-4 mx-auto max-w-screen-xl text-center py-10 lg:py-40">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+          Welcome to Pharma Assist
+        </h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+          Welcome to Pharma Assist grading app, the cutting-edge web application
+          designed to revolutionize grading procedures for the Pharmacy
+          Assistant program at our college. Say goodbye to the hassle of
+          pen-and-paper grading methods and embrace the efficiency and accuracy
+          of digital grading. Pharma Assist grading app offers a user-friendly
+          interface tailored to the specific needs of instructors and students
+          within the Pharmacy Assistant program.
+        </p>
+        <button className="btn btn-primary" onClick={handleGetStartedClick}>
+          Get Started
+        </button>
+        {showLoginPage && (
+          <div>
+            <LoginPage />
+          </div>
+        )}
       </div>
-      <footer className="p-4 bg-primary text-white">Footer</footer>
-    </div>
+    </section>
   );
 }
