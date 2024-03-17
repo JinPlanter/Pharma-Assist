978dc4e495d58ae66be3611aab44d6104583e611
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2024-03-16 20:50:33 -0600

    Removed TM
    Minor changes

--- app/about/page.js
+++ app/about/page.js
@@ -59,9 +59,9 @@ export default function About() {
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
           <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2024{" "}
+               2024{" "}
               <a href="" class="hover:underline">
-                Pharma Assist™
+                Pharma Assist
               </a>
               . All Rights Reserved.
             </span>
--- app/page.js
+++ app/page.js
@@ -91,9 +91,9 @@ export default function Home() {
           </div>
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2024{" "}
+             2024{" "}
             <a href="" class="hover:underline">
-              Pharma Assist™
+              Pharma Assist
             </a>
             . All Rights Reserved.
           </span>
