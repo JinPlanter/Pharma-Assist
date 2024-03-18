0b44ba859e86093a6d7a5d8b4a046cf7fca09163
izalu99 <>
2024-03-18 17:30:56 -0600

    added an id for search bar input element

--- app/Students/components/search-bar.js
+++ app/Students/components/search-bar.js
@@ -67,7 +67,8 @@ const SearchBar = ({ data }) => {
   return (
     <div className="p-4 focus:border-primary-600 dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <label className="input flex items-center gap-2 text-gray-900 sm:text-sm rounded-lg  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white light:text-black ">
-            <input 
+            <input
+            id = "search-input"
             type="text" 
             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white light:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             placeholder="Search by name, class or student ID"
