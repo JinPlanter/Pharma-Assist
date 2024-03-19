0b0325cdb72cd5459115f93c5134bc9038843ea0
Sergio Martinez <maikiby123@gmail.com>
2024-03-18 20:15:44 -0600

    created Logout page for demo purposes

---
+++ app/Logout/page.js
@@ -0,0 +1,10 @@
+function LogoutPage() {
+    // This route currently throws 404 and I can't figure out why
+    return (
+    <div>
+      <h1 className="bg-primary text-black text-xl">You are logged out!</h1>
+    </div>
+  );
+}
+
+export default LogoutPage;
