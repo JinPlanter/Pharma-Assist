24b367b8e24417c76e0820457f2001dae8e660e7
izalu99 <>
2024-03-19 23:58:23 -0600

    jest setup done; few search_page_test done

--- __tests__/search_bar_tests/test_1.js
+++
@@ -1,6 +0,0 @@
-    // Renders a search input field with a placeholder text "Search by name, class or student ID"
-    it('should render a search input field with the correct placeholder text', () => {
-        const wrapper = shallow(<SearchBar />);
-        const input = wrapper.find('input');
-        expect(input.prop('placeholder')).to.equal('Search by name, class or student ID');
-      });
---
+++ __tests__/search_page.test.js
@@ -0,0 +1,76 @@
+// test search page
+
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Page from '../app/Students/page';
+
+
+
+// Mocking the fetch function
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([{ id: 1, name: 'John Doe' }, 
+    { id: 2, name: 'Alice' }]),
+    })
+);
+
+
+//custom matcher to handle table format
+const customMatcher = (table, searchText) => {
+    const rows = table.querySelectorAll('tr');
+    const columns = table.querySelectorAll('td');
+    for ( let i = 0; i < rows.length; i++) {
+        const cells = rows[i].querySelectorAll('td');
+        for (let j = 0; j < cells.length; j++) {
+            if (cells[j].textContent === searchText) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+
+//setup the test
+describe('Page component', () => {
+    beforeEach(() => {
+        fetch.mockClear();
+    });
+
+
+    //0. test that the page renders the heading: Student Search Page
+    test('renders Student Search Page', () => {
+        render(<Page />);
+        const heading = screen.getByText(/Student Search Page/i);
+        expect(heading).toBeInTheDocument();
+    });
+
+
+
+    // 1. test that the page renders the search bar component
+    test('renders the search bar component', async() => {
+        let component;
+        await act(async () => {
+            component = render(<Page />);
+        });
+
+        expect(component.getByPlaceholderText("Search by name, class or student ID")).toBeInTheDocument();
+    });
+
+
+
+    // 2. test that the search bar allows input
+    test('allows input in the search bar', async() => {
+        let component;
+        await act(async () => {
+            component = render(<Page />);
+        });
+
+        const searchBar = component.getByPlaceholderText("Search by name, class or student ID");
+        await act(async() => {
+            fireEvent.change(searchBar, { target: { value: 'John' } });
+        });
+        expect(searchBar.value).toBe('John');
+    });
+
+});
