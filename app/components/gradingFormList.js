733ba47969b6fab741573f3f1916bb111a887947
Jin <jin.her@edu.sait.ca>
2024-03-26 07:32:46 -0600

    Flowbite license added and file movement

--- app/components/gradingFormList.js
+++ app/components/gradingFormList.js
@@ -1,133 +1,133 @@
 import React, { useState, useEffect } from "react";
 
 const GradingFormList = () => {
-  const [gradingForms, setGradingForms] = useState([]);
-  const [editingStudent, setEditingStudent] = useState(null);
-  const [editedData, setEditedData] = useState({});
-  const [refreshTimestamp, setRefreshTimestamp] = useState(Date.now());
+    const [gradingForms, setGradingForms] = useState([]);
+    const [editingStudent, setEditingStudent] = useState(null);
+    const [editedData, setEditedData] = useState({});
+    const [refreshTimestamp, setRefreshTimestamp] = useState(Date.now());
 
-  useEffect(() => {
-    const fetchGradingForms = async () => {
-      try {
-        const response = await fetch("/api/getFileContent");
-        const data = await response.json();
-        setGradingForms(data);
-      } catch (error) {
-        console.error("Error fetching grading forms:", error);
-      }
+    useEffect(() => {
+        const fetchGradingForms = async () => {
+            try {
+                const response = await fetch("/api/getFileContent");
+                const data = await response.json();
+                setGradingForms(data);
+            } catch (error) {
+                console.error("Error fetching grading forms:", error);
+            }
+        };
+
+        fetchGradingForms();
+    }, [refreshTimestamp]); // Refresh when refreshTimestamp changes
+
+    const handleEditStudent = (student) => {
+        setEditingStudent(student);
+        setEditedData(student);
     };
 
-    fetchGradingForms();
-  }, [refreshTimestamp]); // Refresh when refreshTimestamp changes
+    const handleInputChange = (e, field) => {
+        setEditedData((prevData) => ({
+            ...prevData,
+            [field]: e.target.value,
+        }));
+    };
 
-  const handleEditStudent = (student) => {
-    setEditingStudent(student);
-    setEditedData(student);
-  };
+    const handleSaveStudent = async () => {
+        try {
+            const encodedUsername = encodeURIComponent(editingStudent.username);
+            const response = await fetch(`/api/updateStudent/${encodedUsername}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(editedData),
+            });
 
-  const handleInputChange = (e, field) => {
-    setEditedData((prevData) => ({
-      ...prevData,
-      [field]: e.target.value,
-    }));
-  };
+            if (response.ok) {
+                console.log("Student updated successfully");
+                setEditingStudent(null);
+                setEditedData({});
+                setRefreshTimestamp(Date.now()); // Trigger refresh
+            } else {
+                console.error("Failed to update student");
+            }
+        } catch (error) {
+            console.error("Error updating student:", error);
+        }
+    };
 
-  const handleSaveStudent = async () => {
-    try {
-      const encodedUsername = encodeURIComponent(editingStudent.username);
-      const response = await fetch(`/api/updateStudent/${encodedUsername}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(editedData),
-      });
+    const downloadGradingForm = (form) => {
+        const fileData = JSON.stringify(form.fileContentJson, null, 2);
+        const blob = new Blob([fileData], { type: "application/json" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `gradingForm_${form._id}.json`;
+        link.click();
+    };
 
-      if (response.ok) {
-        console.log("Student updated successfully");
-        setEditingStudent(null);
-        setEditedData({});
-        setRefreshTimestamp(Date.now()); // Trigger refresh
-      } else {
-        console.error("Failed to update student");
-      }
-    } catch (error) {
-      console.error("Error updating student:", error);
-    }
-  };
-
-  const downloadGradingForm = (form) => {
-    const fileData = JSON.stringify(form.fileContentJson, null, 2);
-    const blob = new Blob([fileData], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = `gradingForm_${form._id}.json`;
-    link.click();
-  };
-
-  return (
-    <div>
-      <h2>Grading Forms</h2>
-      {gradingForms.length === 0 ? (
-        <p>No grading forms found.</p>
-      ) : (
-        <ul>
-          {gradingForms.map((form, index) => (
-            <li key={index}>
-              <div>
-                <h3>Grading Form {index + 1}</h3>
+    return (
+        <div>
+            <h2>Grading Forms</h2>
+            {gradingForms.length === 0 ? (
+                <p>No grading forms found.</p>
+            ) : (
                 <ul>
-                  {form.fileContentJson.map((student, idx) => (
-                    <li key={idx}>
-                      {editingStudent === student ? (
-                        <div>
-                          <p>
-                            Username: {student.username} First Name:{" "}
-                            {student.firstName} Last Name: {student.lastName}{" "}
-                            Email: {student.email}
-                          </p>
-                          <input
-                            type="text"
-                            value={editedData.firstName}
-                            onChange={(e) => handleInputChange(e, "firstName")}
-                          />
-                          <input
-                            type="text"
-                            value={editedData.lastName}
-                            onChange={(e) => handleInputChange(e, "lastName")}
-                          />
-                          <input
-                            type="email"
-                            value={editedData.email}
-                            onChange={(e) => handleInputChange(e, "email")}
-                          />
-                          <button onClick={handleSaveStudent}>Save</button>
-                        </div>
-                      ) : (
-                        <div>
-                          <span>Username: {student.username} </span>
-                          <span>First Name: {student.firstName} </span>
-                          <span>Last Name: {student.lastName} </span>
-                          <span>Email: {student.email} </span>
-                          <button onClick={() => handleEditStudent(student)}>
-                            Edit
-                          </button>
-                        </div>
-                      )}
-                    </li>
-                  ))}
+                    {gradingForms.map((form, index) => (
+                        <li key={index}>
+                            <div>
+                                <h3>Grading Form {index + 1}</h3>
+                                <ul>
+                                    {form.fileContentJson.map((student, idx) => (
+                                        <li key={idx}>
+                                            {editingStudent === student ? (
+                                                <div>
+                                                    <p>
+                                                        Username: {student.username} First Name:{" "}
+                                                        {student.firstName} Last Name: {student.lastName}{" "}
+                                                        Email: {student.email}
+                                                    </p>
+                                                    <input
+                                                        type="text"
+                                                        value={editedData.firstName}
+                                                        onChange={(e) => handleInputChange(e, "firstName")}
+                                                    />
+                                                    <input
+                                                        type="text"
+                                                        value={editedData.lastName}
+                                                        onChange={(e) => handleInputChange(e, "lastName")}
+                                                    />
+                                                    <input
+                                                        type="email"
+                                                        value={editedData.email}
+                                                        onChange={(e) => handleInputChange(e, "email")}
+                                                    />
+                                                    <button onClick={handleSaveStudent}>Save</button>
+                                                </div>
+                                            ) : (
+                                                <div>
+                                                    <span>Username: {student.username} </span>
+                                                    <span>First Name: {student.firstName} </span>
+                                                    <span>Last Name: {student.lastName} </span>
+                                                    <span>Email: {student.email} </span>
+                                                    <button onClick={() => handleEditStudent(student)}>
+                                                        Edit
+                                                    </button>
+                                                </div>
+                                            )}
+                                        </li>
+                                    ))}
+                                </ul>
+                                <button onClick={() => downloadGradingForm(form)}>
+                                    Download
+                                </button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
-                <button onClick={() => downloadGradingForm(form)}>
-                  Download
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+            )}
+        </div>
+    );
 };
 
 export default GradingFormList;
