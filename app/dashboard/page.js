bf273b91258c73b654e9dbd1c78bbc02f9a2dc68
Jin <jin.her@edu.sait.ca>
2024-04-01 18:01:49 -0600

    Revert "Merge branch 'main' of https://github.com/JinPlanter/Pharma-Assist into newGFormIntegration"
    This reverts commit e48d969c04931666ff7e6aa7654ad3953663700a, reversing
    changes made to 0b73d617c8a1db4bb27d3109424bcef069d62436.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,50 +1,46 @@
 "use client";
 import { useState } from "react";
-import Grading from "../GradingForm4/page";
+import Grading from "../Grading/page";
 import LoginPage from "../Login/page";
 import Students from "../Students/page";
 import ImportFile from "../ImportFile/page";
 import LogoutPage from "../Logout/page";
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/navbar";
-import DisplayFilePage from "../displayFile/page";
-import UploadClassList from "../uploadclasslist/page";
 
 function Dashboard() {
-  const [activeComponent, setActiveComponent] = useState("");
+    const [activeComponent, setActiveComponent] = useState("");
 
-  const handleNavigationClick = (componentName) => {
-    setActiveComponent(componentName);
-  };
-  // This is terrible and I hate it but it works ¯\_(ツ)_/¯
-  const renderComponent = () => {
-    switch (activeComponent) {
-      case "GradingForm4":
-        return <Grading />;
-      case "Students":
-        return <Students />;
-      case "ImportFile":
-        return <ImportFile />;
-      case "LogOut":
-        return <LogoutPage />;
-      case "displayFile":
-        return <DisplayFilePage />;
-      case "uploadclasslist":
-        return <UploadClassList />;
-      default:
-        return <Grading />;
-    }
-  };
+    const handleNavigationClick = (componentName) => {
+        setActiveComponent(componentName);
+    };
+    // This is terrible and I hate it but it works ¯\_(ツ)_/¯
+    const renderComponent = () => {
+        switch (activeComponent) {
+            case "Grading":
+                return <Grading />;
+            case "Login":
+                return <LoginPage />;
+            case "Students":
+                return <Students />;
+            case "Import File":
+                return <ImportFile />;
+            case "Log Out":
+                <LogoutPage />
+            default:
+                return <Grading />;
+        }
+    };
 
-  return (
-    <div className="overflow-auto h-screen w-full bg-white border-b border-gray-200 dark:bg-gray-700 ">
-      <Navbar />
-      <div className="w-full flex flex-row">
-        <Sidebar onNavigationClick={handleNavigationClick} />
-        <div className="w-full drawer-content">{renderComponent()}</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="overflow-auto h-screen w-full bg-white border-b border-gray-200 dark:bg-gray-700 ">
+            <Navbar />
+            <div className="w-full flex flex-row">
+                <Sidebar onNavigationClick={handleNavigationClick} />
+                <div className="w-full drawer-content">{renderComponent()}</div>
+            </div>
+        </div>
+    );
 }
 
 export default Dashboard;
