ace9dfdcefe40cbf1bab6ca812618fd870002cc8
izalu99 <iza.lumpio@edu.sait.ca>
2024-04-06 17:05:50 -0600

    made scrolling size of classList responsive

--- app/components/classList.js
+++ app/components/classList.js
@@ -1,20 +1,49 @@
 // Import the React module
-import React from "react";
+import React, {useState, useEffect } from "react";
 import { removeHashtag } from "../Students/components/search-bar";
 
+
+function useWindowSize(){
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        function handleResize(){
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowSize;
+};
+
 // ClassList component that takes in classlist and onStudentClick as props
-const ClassList = ({ classlist, onStudentClick }) => {
-    // JSX rendering for the ClassList component
+const ClassList = ({ classlist, onStudentClick, selectedStudent }) => {
+    
+    const size = useWindowSize();
 
     return (
-        <ul className="classList" style={{height: '80vh', overflowY: 'auto'}}>
+        <ul className="" 
+        style={{
+            height: size.width <= 768 ? '30vh': '80vh', 
+            overflowY: 'auto'}}>
             {" "}
             {/* Added margin-top here for styling */}
             {/* Map through classlist to create list items for each student */}
             {classlist.map((student) => (
                 <li
                     key={removeHashtag(student.username)}
-                    className="classListItem"
+                    className={`classListItem ${selectedStudent === student ? "bg-red-500" : ""}`}
                     onClick={() => onStudentClick(student)}
                 >
                     {/* Display the name of the student */}
