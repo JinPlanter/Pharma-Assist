b32558d4667ceadaf6fcea20ce99c2261e0a668f
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-04-11 18:33:06 -0600

    added PUT function for updating Student info

--- app/api/Students/[username]/route.js
+++ app/api/Students/[username]/route.js
@@ -46,14 +46,86 @@ export const GET = async (request, response) => {
             );
         }
 
-
-        //return new Response(JSON.stringify(studentData), { status: 200 });
         return NextResponse.json(studentData, { status: 200 });
 
     } catch (error) {
         console.log("Error: ", error);
         return NextResponse.error(
             new Error("Unable to fetch student data from classList collection"),
+            { status: 500 }
+        );
+    }
+}
+
+
+
+//update student data
+export const PUT = async (request, response) => {
+    try {
+        // connect to db
+        const client = await clientPromise;
+        const db = client.db("capstone"); //db name
+
+        //check if connection is successful
+        if (!db) {
+            return NextResponse.error(
+                new Error("Unable to connect to database"),
+                { status: 500 }
+            );
+        } else{
+            console.log("Connected to database successfully");
+        }
+
+        
+
+        // read request body 
+        const chunks = [];
+        for await (const chunk of request.body) {
+            chunks.push(chunk);
+        }
+
+        // parse the request body as JSON
+        const updatedData = JSON.parse(Buffer.concat(chunks).toString());
+        console.log("Updated data: ", updatedData);
+        
+
+        
+        //update student data in collection
+        const students = await db.collection("classlist")
+        // create a filter to find the student with the username in the request
+        console.log("fileContentJson.username: ", `#${response.params.username}` );
+        const filter = { "fileContentJson": { $elemMatch: { "username": `#${response.params.username}` } } };
+
+        // specify the update to set a value for the student with the username in the request
+        // updatedData is a json object that contains the updated student data
+        // use .$. to specify the index of the array element to update
+        const updateDoc = {
+            $set: { "fileContentJson.$.firstName": updatedData.firstName,
+                    "fileContentJson.$.lastName": updatedData.lastName,
+                    "fileContentJson.$.email": updatedData.email
+                },
+        };
+        
+        // update the first document that matches the filter
+        const result = await students.updateOne(filter, updateDoc);
+
+        if (result) {
+            // check if updated
+            const updatedStudent = await students.findOne(filter);
+            console.log("Updated student: ", updatedStudent);
+            // if the update was successful, return a success response
+            return NextResponse.json({ message: "Student data updated successfully" }, { status: 200 });
+        } else{
+            // if no document was modified, return a not found response
+            return NextResponse.error(
+                new Error("No student found"),
+                { status: 404 }
+            );
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        return NextResponse.error(
+            new Error("Unable to update student data"),
             { status: 500 }
         );
     }
